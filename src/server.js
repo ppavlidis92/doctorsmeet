70ba48a51f5642d8737860dfce05602fcd71fa29
ppavlidis92 <pavlos.pvlds@gmail.com>
2022-12-06 00:05:53 +0200

    remove:

--- src/server.js
+++
@@ -1,8 +0,0 @@
-var app = require('./app');
-require('dotenv').config();
-
-const port = process.env.PORT;
-
-var server = app.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
-});
